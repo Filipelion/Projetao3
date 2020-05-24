d45f9abd6d5f26a22a3be5507d5e8878f6843db8
filipelion <14293424+Filipelion@users.noreply.github.com>
2020-05-24 01:03:39 -0300

    Rename to Projetao3

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Test laucher'),
+      home: MyHomePage(title: 'Test laucher 3'),
     );
   }
 }
