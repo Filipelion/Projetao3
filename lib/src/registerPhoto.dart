08bf2788a7613e7ee849aaa151db09be71007be4
filipelion <14293424+Filipelion@users.noreply.github.com>
2020-06-06 22:31:25 -0300

    inicio da tela register

--- lib/src/registerPhoto.dart
+++ lib/src/registerPhoto.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+class ScreenArguments {
+  final String phone;
+  final String name;
+  final String gender;
+
+  ScreenArguments(this.phone, this.name, this.gender);
+}
+
+
+
 class RegisterPhotoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("RegisterPhoto Page")),
+        appBar: AppBar(title: Text("RegisterPhoto Page"),
+        backgroundColor: Color(0xFFFFBA00)       
+        ),
         body: Center(
             child: 
             Column(
@@ -23,7 +35,7 @@ class RegisterPhotoPage extends StatelessWidget {
                   child: Text("Passar reto"),
                   onPressed: () 
                   {
-                    Navigator.pushNamed(context, '/login');
+                    //Navigator.pushNamed(context, '/login');
                   }
             ),
           ]
